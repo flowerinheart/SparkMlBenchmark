c39c914bb48089b42bf9c2560170268f1ddf5a56
haojie.ma <mhj1137633684@gmail.com>
2017-02-22 16:29:59 +0800

    move env function to env file in env folder

--- benchmarks/src/main/scala/KmeansBenchmark.scala
+++ benchmarks/src/main/scala/KmeansBenchmark.scala
@@ -15,19 +15,51 @@
  */
 
 
+import org.apache.log4j.{Level, Logger}
 import org.apache.spark.mllib.clustering.{KMeans, KMeansModel}
 import org.apache.spark.mllib.linalg.{Vector, Vectors}
+import org.apache.spark.mllib.util.KMeansDataGenerator
 import org.apache.spark.rdd.RDD
 import org.apache.spark.{SparkConf, SparkContext}
 
+object KmeansDataGen {
+  def run(args: Array[String]) {
+    if (args.length < 5) {
+      println("usage: <datadir> <numPoints> <numClusters> <dimenstion> <scaling factor> [numpar]")
+      System.exit(0)
+    }
+    val conf = new SparkConf
+    conf.setAppName("Spark KMeans DataGen")
+    val sc = new SparkContext(conf)
+
+    val output = args(0)
+    val numCluster = args(1).toInt
+    val numPoint = args(2).toInt
+    val numDim = args(3).toInt
+    val scaling = args(4).toDouble
+    val defPar = if (System.getProperty("spark.default.parallelism") == null) 2 else System.getProperty("spark.default.parallelism").toInt
+    val numPar = if (args.length > 5) args(5).toInt else defPar
+
+    val data = KMeansDataGenerator.generateKMeansRDD(sc, numPoint, numCluster, numDim, scaling, numPar)
+    data.map(_.mkString(" ")).saveAsTextFile(output)
+
+    sc.stop();
+  }
+
+}
 
 object KmeansApp {
   def main(args: Array[String]) {
-    if (args.length < 4) {
-      println("usage: <input> <output> <numClusters> <maxIterations> <runs> - optional")
+    if (args.length < 8) {
+      println("usage: <numClusters> <maxIterations> <runs> <data_folder> <num_of_points> <DIMENSIONS> <SCALING> <NUM_OFPARATIONS>")
       System.exit(0)
     }
+    KmeansDataGen.run(Array(args(3), args(0), args(4), args(5), args(6), args(7)))
+
     val metrics = new SparkMetrics[RDD[Vector], KMeansModel]("KMneas")
+    //generate data
+
+
     //parse arguments
     metrics.onParseArg({
       val map = collection.mutable.HashMap[String, String]()
--- bin/run-all.sh
+++ bin/run-all.sh
@@ -3,43 +3,24 @@ DIR=`dirname "$0"`
 DIR=`cd "$DIR"; pwd`
 . "${DIR}/config.sh"
 
-function kmeans_env() {
-  JAR="${DIR}/../benchmarks/target/spark.benchmarks-${BENCH_VERSION}.jar"
 
-  NUM_RUN=1
-  NUM_OF_CLUSTERS=200
-  MAX_ITERATION=300
-
-  DATA_FOLDER="${DIR}/../data"
-  NUM_OF_POINTS=100000
-  DIMENSIONS=2000
-  SCALING=0.6
-  NUM_OF_PARTITIONS=20
-  CLASS="KmeansApp"
-  OPTION=" ${NUM_OF_CLUSTERS} ${MAX_ITERATION} ${NUM_RUN} ${DATA_FOLDER} ${NUM_OF_POINTS} ${DIMENSIONS} ${SCALING} ${NUM_OF_PARTITIONS}"
-}
 function run_benchmark() {
 # set algorithm enviroment variable
-case $1 in
-"Kmeans")
-  kmeans_env
-  ;;
-*)
-  echo "Unknown Algorithm"
-  ;;
-esac
-#
+. "$DIR/../env/$1"
 #DU ${INPUT_HDFS} SIZE
 # prepare spark opt
 set_gendata_opt
 set_run_opt
-set_MKL $1
+set_MKL --MKL
 set_Java_OPT
 setup
-RM ${OUTPUT_HDFS}
-# (Optional procedure): free page cache, dentries and inodes.
-# purge_data "${MC_LIST}"
-echo_and_run sh -c " ${SPARK_HOME}/bin/spark-submit --name $CLASS --class $CLASS --master ${SPARK_MASTER} ${YARN_OPT} ${SPARK_OPT} $JAR ${OPTION}
+
+# remove data file
+#RM ${OUTPUT_HDFS}
+
+echo_and_run sh -c " ${SPARK_HOME}/bin/spark-submit --name ${CLASS} --class ${CLASS} --master ${SPARK_MASTER} ${YARN_OPT} ${SPARK_OPT} ${JAR} ${OPTION}"
 }
 
-run_benchmark "Kmeans"
+for file in ${DIR}/../env/*; do
+  run_benchmark `basename $file`
+done
